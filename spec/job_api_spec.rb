838c9d4d67e3cb6f507d861c00e40eec2d3043a6
Loudwige Odice <55907221+Gaby077@users.noreply.github.com>
2021-11-01 18:18:56 +0800

    Testing tooling and Refactoring

--- spec/job_api_spec.rb
+++ spec/job_api_spec.rb
@@ -1,31 +1,37 @@
 # frozen_string_literal: true
 
-require 'minitest/autorun'
-require 'minitest/rg'
-require 'yaml'
-require_relative '../lib/job_api'
+require_relative 'spec_helper'
 
-require 'careerjet/api_client'
-
-SKILL = 'ruby'
-LOCATION = 'london'
-CONFIG = YAML.safe_load(File.read('config/secrets.yml'))
-JOB_TOKEN = CONFIG['API_KEY']
-
-GOOD = YAML.safe_load(File.read('spec/fixtures/job_results.yml'), permitted_classes: [Careerjet::Mash, Hashie::Array])
-
-describe 'Test Career Jet API' do
-  before do
-    @job_data = Jobify::CarrerjetApi.new(JOB_TOKEN).job(SKILL, LOCATION)
-    @good_data = GOOD.jobs.map { |x| Jobify::Job.new(x) }
+describe 'Tests Career Jet API library' do
+  VCR.configure do |c|
+    c.cassette_library_dir = CASSETTES_FOLDER
+    c.hook_into :webmock
+    c.filter_sensitive_data('<API_KEY>') { JOB_TOKEN }
+    c.filter_sensitive_data('<API_KEY_ESC>') { CGI.escape(JOB_TOKEN) }
   end
-  it 'HAPPY: should return correct Job Info' do
-    (0..@good_data.size - 1).each do |i|
-      _(@job_data[i].locations).must_equal @good_data[i].locations
-      _(@job_data[i].date).must_equal @good_data[i].date
-      _(@job_data[i].company).must_equal @good_data[i].company
-      _(@job_data[i].url).must_equal @good_data[i].url
-      _(@job_data[i].title).must_equal @good_data[i].title
+  before do
+    VCR.insert_cassette CASSETTE_FILE,
+                        record: :new_episodes,
+                        match_requests_on: %i[method uri headers]
+  end
+  after do
+    VCR.eject_cassette
+  end
+
+  describe 'Job Information' do
+    before do
+      @job_data = Jobify::CarrerjetApi.new(JOB_TOKEN).job(SKILL, LOCATION)
+      @good_data = GOOD.jobs.map { |x| Jobify::Job.new(x) }
+    end
+    it 'HAPPY: should return correct Job Info' do
+      (0..@good_data.size - 1).each do |i|
+        _(@job_data[i].locations).must_equal @good_data[i].locations
+        _(@job_data[i].date).must_equal @good_data[i].date
+        _(@job_data[i].company).must_equal @good_data[i].company
+        _(@job_data[i].url).must_equal @good_data[i].url
+        _(@job_data[i].title).must_equal @good_data[i].title
+        _(@job_data[i].description).must_equal @good_data[i].description
+      end
     end
   end
 end
