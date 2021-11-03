1c60a88d0c7733e927c2b169c9208d7702575823
Thierry <marcelinthierry@gmail.com>
2021-11-03 14:11:35 +0800

    Added YAML file

--- lib/resume_info.rb
+++ lib/resume_info.rb
@@ -1,11 +1,15 @@
+# frozen_string_literal: true
+
 require 'curb'
 require 'yaml'
+require 'http'
 
 config = YAML.safe_load(File.read('config/secrets.yml'))
 
-file = File.open('lib/resume.pdf', 'rb', &:read)
 c = Curl::Easy.new('https://api.affinda.com/v1/resumes/')
 c.headers['Authorization'] = "Bearer #{config['CV_TOKEN']}"
 c.multipart_form_post = true
 c.http_post(Curl::PostField.file('file', 'lib/resume.pdf'))
-puts c.body
+results = JSON.parse(c.body)
+
+File.write('spec/fixtures/resume_result.yml', results.to_yaml)
