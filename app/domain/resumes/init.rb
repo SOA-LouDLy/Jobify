e30b7a39804bb01539e05d473100d8330319e2a2
Thierry <marcelinthierry@gmail.com>
2022-01-05 16:40:46 +0800

    New Infrastructure

--- app/domain/resumes/init.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: false
-
-%w[entities mappers repositories]
-  .each do |folder|
-  Dir.glob("#{__dir__}/#{folder}/**/*.rb").each do |ruby_file|
-    require_relative ruby_file
-  end
-end
