201a3788a236867e9cb9c59b681b9bfa1112014a
Malherbe <malherbe.billy@gmail.com>
2022-01-09 16:09:30 +0800

    Add representers and services

--- app/application/services/add_resume.rb
+++ app/application/services/add_resume.rb
@@ -7,29 +7,24 @@ module Jobify
     # Transaction to upload a resume to Affinda API to database
     class AddResume
       include Dry::Transaction
-
       # step :check_entered_file
       step :request_resume
-      step :reify_resume
-
+      step :reify_request
       private
-
       def request_resume(input)
         result = Gateway::Api.new(Jobify::App.config)
           .add_resume(input)
-
         result.success? ? Success(result.payload) : Failure(result.message)
       rescue StandardError => e
         puts "#{e.inspect} + '\n' + #{e.backtrace}"
         Failure('Cannot add resumes right now; please try again later')
       end
-
-      def reify_resume(resume_json)
-        Representer::Resume.new(OpenStruct.new)
-          .from_json(resume_json)
-          .then { |resume| Success(resume) }
+      def reify_request(request_json)
+        Representer::UploadRequest.new(OpenStruct.new)
+          .from_json(request_json)
+          .then { |request| Success(request) }
       rescue StandardError
-        Failure('Error in the resume -- please try again')
+        Failure('Error in the request -- please try again')
       end
     end
   end
